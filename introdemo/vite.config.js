f070f5f8086153c788e0d9ec90ed36c913fa090a
alexjoel42 <alex.copperman@opentrons.com>
2024-12-09 18:30:47 -0500

    let's try removing the config

--- introdemo/vite.config.js
+++ introdemo/vite.config.js
@@ -5,7 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Selector_Practice/', // Set this to match your repo name
-  build: {
-    outDir: 'build', // Ensure this matches the gh-pages deploy target
-  },
+
 });
